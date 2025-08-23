1deb6aec9d3225e697e27c28713de06bc470cfff
Guilherme <guilhermediasvillanova@gmail.com>
2025-08-23 15:14:36 -0300

    feat: estrutura de rotas mais pagina de register pre-pronta

---
+++ Front-react/src/pages/register/Register.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import RegisterField from '../../componentes/registerField'
+import PasswordField from '../../componentes/passwordField'
+import './Register.css'
+import Hearder from '../../componentes/Hearder'
+
+function Register() {
+    return (
+        <>
+            <Hearder />
+            <div className='register-container'>
+
+                <div id='text'>
+                    <h2 id='big-txt'>
+                        Create an account:
+                    </h2>
+                    <h3 id='small-txt'>
+                        Transform the future now.Your help will <br />
+                        make a difference!
+                    </h3>
+                </div>
+
+                <div id='register'>
+                    <RegisterField texto='Email' />
+                    <RegisterField texto='Cpf' />
+                    <PasswordField texto='Password' />
+                    <PasswordField texto='Confirm password' />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Register
--- Front-react/src/routes/index.jsx
+++ Front-react/src/routes/index.jsx
@@ -1,9 +1,14 @@
 import Home from "../pages/home/Home"
+import Register from "../pages/register/Register";
 
 const routes = [
   {
     path: "/home",
     element: <Home />
+  },
+  {
+    path: "/regis",
+    element: <Register />
   }
 ];
 
