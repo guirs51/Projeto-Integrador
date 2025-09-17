322197d0d132fb00097857f35833bcf2bbb5c0ae
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-09-17 17:21:11 -0300

    update home

--- Front-react/src/pages/register/Register.jsx
+++ Front-react/src/pages/register/Register.jsx
@@ -1,37 +1,86 @@
 import React from 'react'
-import RegisterField from '../../componentes/registerField'
-import PasswordField from '../../componentes/passwordField'
 import './Register.css'
-import Hearder from '../../componentes/Hearder'
-import Button from '../../componentes/Button'
+import { FaGooglePlusG } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 
 
 function Register() {
+
+    const container = document.getElementById('container');
+    const register = document.getElementById('register');
+    const logina = document.getElementById('login');
+
+
+    function regis() {
+        register.addEventListener('click', () => {
+            container.classList.add("active");
+        })
+    }
+
+    function login() {
+        logina.addEventListener('click', () => {
+            container.classList.remove("active");
+        })
+    }
+
+
+
     return (
         <>
-            <Hearder />
-            <div className='register-container'>
-
-                <div id='text'>
-                    <h2 id='big-txt'>
-                        Create an  <br /> account:
-                    </h2>
-                    <h3 id='small-txt'>
-                        Transform the future now.Your help will <br />
-                        make a difference!
-                    </h3>
+            <div className='container'>
+                <div className='form-container sign-up'>
+                    <form>
+                        <h1> Create Account</h1>
+                        <div className='social-icons'>
+                            <a className="icon" href=""><FaGooglePlusG /></a>
+                            <a className="icon" href=""><FaFacebookF /></a>
+                            <a className="icon" href=""><FaGithub /></a>
+                            <a className="icon" href=""><FaLinkedinIn /></a>
+                        </div>
+                        <span>or use your email for registartion</span>
+                        <input type="text" placeholder='Name' />
+                        <input type="email" placeholder='Email' />
+                        <input type="password" placeholder='password' />
+                        <button>Sign Up</button>
+                    </form>
                 </div>
-
-                <div id='register'>
-                    <RegisterField texto='Email:' />
-                    <RegisterField texto='CPF:' />
-                    <PasswordField texto='Password:' />
-                    <PasswordField texto='Confirm password:' />
-                    <Button texto='Next'/>
+                <div className='form-container sign-in'>
+                    <form>
+                        <h1>Sign in</h1>
+                        <div className='social-icons'>
+                            <a className="icon" href=""><FaGooglePlusG /></a>
+                            <a className="icon" href=""><FaFacebookF /></a>
+                            <a className="icon" href=""><FaGithub /></a>
+                            <a className="icon" href=""><FaLinkedinIn /></a>
+                        </div>
+                        <span>or use your email passsword</span>
+                        <input type="email" placeholder='Email' />
+                        <input type="password" placeholder='Password' />
+                        <a href="">Forget Your Password</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+                <div className='toggle-container'>
+                    <div className='toggle'>
+                        <div className='toggle-panel toggle-left'>
+                            <h1>Welcome Back!</h1>
+                            <p>Enter your personal details to use all of site feature</p>
+                            <button className='hidden' id='login' onClick={login}>Sign In</button>
+                        </div>
+                        <div className='toggle-panel toggle-right'>
+                            <h1>Hello</h1>
+                            <p>Register  with your personal details to use all site features</p>
+                            <button className='hidden' id='register' onClick={regis}>Sign Up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
     )
+
 }
 
 export default Register
