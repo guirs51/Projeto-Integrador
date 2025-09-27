3bd54cec84c1b4b82dbf8971d2a20506b64be6d4
Guilherme <guilhermediasvillanova@gmail.com>
2025-09-27 11:16:36 -0300

    feat: nova tela login-registro

--- Front-react/src/pages/register/Register.jsx
+++ Front-react/src/pages/register/Register.jsx
@@ -4,30 +4,60 @@ import PasswordField from '../../componentes/passwordField'
 import './Register.css'
 import Hearder from '../../componentes/Hearder'
 import Button from '../../componentes/Button'
+import { useState } from 'react'
 
 
 function Register() {
+
+    const [activeForm, setActiveForm] = useState('cadastro');
+
+    function login(){
+        setActiveForm('login');
+    }
+
+    function cadastro(){
+        setActiveForm('cadastro');
+    }
+
+    const cardClass = `card ${activeForm === 'cadastro' ? 'cadastroActive' : 'loginActive'}`;
+
     return (
         <>
-            <Hearder />
-            <div className='register-container'>
-
-                <div id='text'>
-                    <h2 id='big-txt'>
-                        Create an  <br /> account:
-                    </h2>
-                    <h3 id='small-txt'>
-                        Transform the future now.Your help will <br />
-                        make a difference!
-                    </h3>
-                </div>
-
-                <div id='register'>
-                    <RegisterField texto='Email:' />
-                    <RegisterField texto='CPF:' />
-                    <PasswordField texto='Password:' />
-                    <PasswordField texto='Confirm password:' />
-                    <Button texto='Next'/>
+            <div className='container'>
+                <div className={cardClass} id='card'>
+                    <div className='esquerda'>
+                        <div className='formLogin'>
+                            <h1>Login</h1>
+                            <form action="">
+                                <input type="text" placeholder='Email' />
+                                <input type="text" placeholder='Password' />
+                                <button>Entrar</button>
+                            </form>
+                        </div>
+                        <div className='login'>
+                            <h1>Ja tem uma conta?</h1>
+                            <p>entre agora em sua conta</p>
+                            <button onClick={login}>Login</button>
+                        </div>
+                    </div>
+                    <div className='direita'>
+                        <div className='formCadastro'>
+                            <h1>Cadastro</h1>
+                            <form action="">
+                                <input type="text" placeholder='Nome' />
+                                <input type="text" placeholder='Email'/>
+                                <input type="Password" placeholder='Password'/>
+                                <button>Cadastrar</button>
+                            </form>
+                        </div>
+                        <div className='cadastro'>
+                            <h1>Não tem uma Conta</h1>
+                            <p>cria sua conta agora!</p>
+                            <button onClick={cadastro}>Cadastrar</button>
+                        </div>
+                    </div>
+                    <div className='background'>
+                    </div>
                 </div>
             </div>
         </>
