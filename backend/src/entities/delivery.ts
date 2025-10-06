fed6e98ddbdb9375e8f28e97bf8ac5cb170212cc
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-10-06 17:09:07 -0300

    add

---
+++ backend/src/entities/delivery.ts
@@ -0,0 +1,21 @@
+import { Entity,PrimaryGeneratedColumn,Column,ManyToOne} from "typeorm";
+import { User } from "./User";
+
+@Entity('delivery')
+export class Delivery{
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column({length: 100})
+    deliveryLocal:string
+
+    @Column({length:100})
+    materialType:string
+
+    @Column()
+    deliveryDate:Date
+
+    @ManyToOne(() => User,user => user.delivery)
+    user:User
+
+}
