30b54e899d62cd18157b894da95b987b5b26e7e9
Nathalia Minossi <minossinah1@gmail.com>
2025-10-11 12:29:25 -0300

    up

--- Front-react/src/pages/register/Register.jsx
+++ Front-react/src/pages/register/Register.jsx
@@ -1,35 +1,12 @@
-import React from 'react'
-import RegisterField from '../../componentes/registerField'
-import PasswordField from '../../componentes/passwordField'
+
 import './Register.css'
-import Hearder from '../../componentes/Hearder'
-import Button from '../../componentes/Button'
+
 
 
 function Register() {
     return (
         <>
-            <Hearder />
-            <div className='register-container'>
-
-                <div id='text'>
-                    <h2 id='big-txt'>
-                        Create an  <br /> account:
-                    </h2>
-                    <h3 id='small-txt'>
-                        Transform the future now.Your help will <br />
-                        make a difference!
-                    </h3>
-                </div>
-
-                <div id='register'>
-                    <RegisterField texto='Email:' />
-                    <RegisterField texto='CPF:' />
-                    <PasswordField texto='Password:' />
-                    <PasswordField texto='Confirm password:' />
-                    <Button texto='Next'/>
-                </div>
-            </div>
+           
         </>
     )
 }
