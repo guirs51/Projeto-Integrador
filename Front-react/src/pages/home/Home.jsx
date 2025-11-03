20103a2a6bf07bd2a6e0add9beebe0e2b368875a
NATHALIA MINOSSI OLIVEIRA <04987029014@senacrs.edu.br>
2025-11-03 16:14:10 -0300

    up home/regis

--- Front-react/src/pages/home/Home.jsx
+++ Front-react/src/pages/home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <div className='div-pai'>
         <span className='box-branca'><h3 className='home-title'>Recicle +</h3> <span className='home-ball'></span> </span>
         <h1 className='h1-home'>Create your  <br />account or log in!</h1>
-        <p className='home-p'>rascunho apresentado site</p>
+        <p className='home-p'>Welcome to Recicle +, it's your time to make <br /> the diference!</p>
         <div className='home-buttons'>
           <a href="/regis"><button className='home-botao'><h6 className='home-h6'>Register</h6></button></a>
           <a href="/login"><button className='home-botao'><h6 className='home-h6'>Login</h6></button></a>
