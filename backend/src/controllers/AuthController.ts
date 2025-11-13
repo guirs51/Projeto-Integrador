f55768dfa819d9e5e5d6be6184deb84b93a2ba7e
Guilherme <guilhermediasvillanova@gmail.com>
2025-11-12 21:33:49 -0300

    feat: JWT feito, e colocado em duas rotas

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,36 +1,38 @@
-import { Response, Request } from "express";
-import { UserService } from "../services/UserService";
-import { User } from "../entities/User";
+import { Request, Response } from 'express'
+import { UserService } from '../services/UserService'
+import { generateToken } from '../utils/jwt' // Importa a função que gera o JWT
 
-const service = new UserService();
+const service = new UserService()
 
 export class AuthController {
-
-    async register(req: Request, res: Response){
-        try{
+    async register(req: Request, res: Response) {
+        try {
             const user = await service.create(req.body)
             res.status(201).json(user)
-        }catch (error: any){
-            res.status(400).json({mensagem: error.mensagem})   
+        } catch (e: any) {
+            res.status(400).json({ message: e.message })
         }
     }
 
-    async login(req: Request, res: Response){
-        try{
-            const {email, password} = req.body;
+    async login(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body
+            const user = await service.findEmail(email)
+            if (!user) return res.status(404).json({ message: 'Usuário não encontrado' })
 
-            const user = service.findEmail(email);
-            if(!user) return res.status(404).json({mensagem: "Usuario não encontrado!"})
+            const valid = await user.validatePassword(password)
+            if (!valid) return res.status(401).json({ message: 'Senha inválida' })
 
-            const valid = (await user).validatePassword(password)
-            if(!valid) return res.status(401).json({mensagem: "Senha invaldia"});
-            
-            const safe: any = {...user}
+            const safe: any = { ...user }
             delete safe.password
 
-            res.status(201).json({user: safe})
-        }catch (error: any){
-            res.status(400).json({mensagem: error.mensagem})
+
+            const token = generateToken({ id: user.id, email: user.email })
+
+            res.json({ user: safe, token })
+        } catch (e: any) {
+            res.status(400).json({ message: e.message })
         }
     }
+
 }
--- backend/src/routers/user.routes.ts
+++ backend/src/routers/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/UserController'
 import { DeliveryController } from '../controllers/DeliveryController'
+import { authMiddleware } from '../middlewares/authMiddleware'
 
 const router = Router()
 const controller = new UserController()
 const controllerDelivery = new DeliveryController()
 
-router.get('/', controller.list.bind(controller))
+router.get('/', authMiddleware, controller.list.bind(controller))
 router.get('/:id', controller.getById.bind(controller))
 router.post('/', controller.create.bind(controller))
 router.put('/:id', controller.update.bind(controller))
@@ -14,7 +15,7 @@ router.delete('/:id', controller.remove.bind(controller))
 
 // rotas para o user criar os deliverys
 
-router.post("/delivery/", controllerDelivery.create.bind(controllerDelivery));
+router.post("/create/delivery/",authMiddleware, controllerDelivery.create.bind(controllerDelivery));
 router.get('/delivery/', controllerDelivery.list.bind(controllerDelivery))
 router.get('/delivery/:id', controllerDelivery.getById.bind(controller));
 router.put('delivery/:id', controllerDelivery.update.bind(controllerDelivery))
