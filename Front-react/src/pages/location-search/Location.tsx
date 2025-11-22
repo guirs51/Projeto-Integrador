5ed5a6cb5e8ddb1e7ff67a0c8f1335196b1c8148
Nathalia Minossi <minossinah1@gmail.com>
2025-11-22 16:51:14 -0300

    tela de buscar localizaçao atualizada

--- Front-react/src/pages/location-search/Location.tsx
+++ Front-react/src/pages/location-search/Location.tsx
@@ -1,34 +1,100 @@
-import { Search, X } from "lucide-react";
+import { Search, X, Sun, Moon, Settings, Home, LogOut, Inbox, BookOpen, CheckSquare, Users } from "lucide-react";
+import { useState } from "react";
+
 import "./Location.css";
 
 export default function Location() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle("dark-mode");
+  }
+
   return (
-    <div className="location-page">
-      <div className="location-card">
+    <>
 
-        {/* Barra de busca */}
-        <div className="search-container">
-          <div className="search-box">
-            <Search className="search-icon" size={18} />
+      <div className="layout " >
 
-            <input
-              type="text"
-              placeholder="Buscar localização..."
-              className="search-input"
-            />
+        <div className="sidebar-container">
+          <aside className="sidebar">
 
-            <button className="clear-btn">
-              <X size={16} />
-            </button>
+
+            <nav className="nav">
+              <a className="nav-item active">
+                <Home size={18} /> Home
+              </a>
+              <a className="nav-item">
+                <Inbox size={18} /> Bonificações
+              </a>
+              <a className="nav-item">
+                <BookOpen size={18} /> Materias
+              </a>
+              <a className="nav-item">
+                <CheckSquare size={18} /> Reciclagens
+              </a>
+              <a className="nav-item">
+                <Users size={18} /> Localização
+              </a>
+            </nav>
+
+
+
+
+
+            <div className="bottom">
+              <div className="section-title">CONFIGURAÇÕES</div>
+
+              <a className="nav-item">
+                <Settings size={18} /> Configurações
+              </a>
+
+              <a className="nav-item logout">
+                <LogOut size={18} /> Sair
+
+              </a>
+              <div className="darkmode-toggle" onClick={toggleDarkMode}>
+                {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+
+              </div>
+
+            </div>
+          </aside>
+        </div>
+
+      <div className="content">
+         <div className="location-page">
+          <div className="location-card">
+
+            {/* Barra de busca */}
+            <div className="search-container">
+              <div className="search-box">
+                <Search className="search-icon" size={18} />
+
+                <input
+                  type="text"
+                  placeholder="Buscar localização..."
+                  className="search-input"
+                />
+
+                <button className="clear-btn">
+                  <X size={16} />
+                </button>
+              </div>
+            </div>
+
+            {/* Área do mapa */}
+            <div className="map-placeholder">
+              <p>Mapa aparecerá aqui...</p>
+            </div>
+
           </div>
         </div>
-
-        {/* Área do mapa */}
-        <div className="map-placeholder">
-          <p>Mapa aparecerá aqui...</p>
-        </div>
-
       </div>
-    </div>
+       
+      </div>
+
+    </>
   );
 }
