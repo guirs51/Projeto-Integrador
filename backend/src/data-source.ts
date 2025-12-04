47163904ad78658f8c934ea83cdee0fe3f54170e
Nathalia Minossi <minossinah1@gmail.com>
2025-12-03 21:09:23 -0300

    layout

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -11,7 +11,7 @@ config()
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3000,
+    port: Number(process.env.DB_PORT) || 3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
